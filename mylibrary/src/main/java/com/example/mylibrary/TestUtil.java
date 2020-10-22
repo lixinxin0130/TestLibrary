2dd769ee7f83a2cf397dad99c154e7067e4af897
li <lixinxin05@sinochem.com>
2020-10-22 14:51:47 +0800

    添加库文件

---
+++ mylibrary/src/main/java/com/example/mylibrary/TestUtil.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+/**
+ * @author LiXin
+ * @date 2020/10/22 0022
+ */
+public class TestUtil {
+    public static int increment() {
+        return 1 + 2;
+    }
+}
